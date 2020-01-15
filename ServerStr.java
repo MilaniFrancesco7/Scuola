cf4b58d5a1be105fc0bc11d13a1c6d5c618ef0cb
MilaniFrancesco7 <MilaniFrancesco7>
2020-01-15 11:46:12 +0100

    Server java update

--- ServerStr.java
+++ ServerStr.java
@@ -16,6 +16,10 @@ public class ServerStr
 	Socket client            = null;  	// Variabile per l'accettazione del client
 	String stringaRicevuta   = null;  	// Stringa di supporto per l'input
 	String stringaModificata = null;  	// Stringa di supporto per l'output
+	String quit	= "quit";
+	int PORTA = 8888;
+	int contaclient = 1;
+	int contaserver = 1;
 	BufferedReader   inDalClient; 
 	DataOutputStream outVersoClient;
 
@@ -23,13 +27,17 @@ public class ServerStr
 	{
 		try 
 		{
-			System.out.println("1 SERVER partito in esecuzione ...");
+			System.out.println("Server numero "+contaserver+" in esecuzione...\n");
+			System.out.println("\tPorta = "+PORTA);
+			System.out.println("\tStabilire una connessione= nc localhost "+PORTA);
+			System.out.println("\tPer uscire inserire: "+quit+"\n");
 			// creo un server sulla porta 8888 
-			server = new ServerSocket(8888);
+			server = new ServerSocket(PORTA);
 			// rimane in attesa di un client 
 			client = server.accept();
 			// chiudo il server per inibire altri client
 			server.close();
+			contaserver++;
 			//associo due oggetti al socket del client per effettuare la scrittura e la lettura 
 			inDalClient = new BufferedReader(new InputStreamReader (client.getInputStream()));
 			outVersoClient = new DataOutputStream(client.getOutputStream());
@@ -43,42 +51,56 @@ public class ServerStr
 		
 		return client;
 	} 
-
+	
 	public void comunica() 
 	{
 		try
 		{
-			while(stringaRicevuta != "f")
-			{
-				
-				// rimango in attesa della riga trasmessa dal client
-				System.out.println("Benvenuto client, scrivi una frase e la trasformo in maiuscolo. Attendo ...");
-				stringaRicevuta = inDalClient.readLine();
-				System.out.println("Ricevuta la stringa dal cliente : "+stringaRicevuta);
-
-				//la modifico e la rispedisco al client  
-				stringaModificata=stringaRicevuta.toUpperCase();
-				System.out.println("Invio la stringa modificata al client ...");
-				outVersoClient.writeBytes(stringaModificata+'\n');
-			}
-
-			//termina elaborazione sul server : chiudo la connessione del client 
-			System.out.println("SERVER: fine elaborazione ");
+			System.out.println("Benvenuto client, scrivi una frase e la trasformo in maiuscolo.");
+			outVersoClient.writeBytes("Server UPPA stringhe\n");
+			System.out.println("\nAttendo la stringa dal cliente");
 			
-			client.close();
+			while(stringaRicevuta != quit)
+			{
+				stringaRicevuta = inDalClient.readLine();	// Rimango in attesa della stringa dal client
+				
+				if(stringaRicevuta.equals(quit))		// Controllo per chiudere la connessione
+				{
+					System.out.println("\n\n\tChiudo la connessione...\n");
+					contaclient = 1;
+					client.close();
+				}
+				else
+				{
+					//modifico la stringa e la rispedisco al client  
+					System.out.println("\nRichiesta numero "+contaclient+"\n"); 
+					System.out.println("Ricevuta la stringa dal cliente : "+stringaRicevuta);
+					contaclient++;
+					stringaModificata=stringaRicevuta.toUpperCase();
+					System.out.println("Invio la stringa modificata al client ...\n");
+					outVersoClient.writeBytes(stringaModificata+'\n');
+					System.out.println("Attendo la stringa dal cliente");
+				}
+				
+				
+				
+			}
 		}  
-		catch (Exception e) 
+		catch (Exception e) 		// Cattura le eccezioni
 		{
-			System.out.println(e.getMessage());
-			System.out.println("Errore durante la connessione...");
-			System.exit(2);
+			//System.out.println(e.getMessage());
+			//System.out.println("Errore durante la connessione...");
 		}
 	}
-
+	
+	public static ServerStr servente = new ServerStr();
+	
 	public static void main(String args[])
 	{
-		ServerStr servente = new ServerStr();
-		servente.attendi ();
-		servente.comunica();
+		while(true)
+		{
+			servente.attendi();
+			servente.comunica();
+		}
 	} 
-}
+} 
